5cd259df3860272e4a46a1615e705e8089aefcc5
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2024-01-31 21:55:20 -0600

    feat(cors implemented)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const entriesRoutes = require('./routes/entries.routes.js');
 
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Routes
